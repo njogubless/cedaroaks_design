1311a9e971c681c691d7edea45e3a3ca99dccef9
njogubless <pnjogubless@gmail.com>
2025-09-11 16:11:00 +0300

    creating the loading screen

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,74 @@
+// Loading Screen
+import 'package:cedaroaks_design/screens/reg_sucess.dart';
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+    _animation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+    
+    _animationController.repeat();
+    
+    // Auto navigate to success after 3 seconds
+    Future.delayed(Duration(seconds: 3), () {
+      if (mounted) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => RegistrationSuccessScreen()),
+        );
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _animation,
+          builder: (context, child) {
+            return Container(
+              width: 60,
+              height: 60,
+              child: CircularProgressIndicator(
+                value: _animation.value,
+                strokeWidth: 4,
+                valueColor: AlwaysStoppedAnimation<Color>(Color(0xFF4F7DF8)),
+                backgroundColor: Color(0xFFE3EAFF),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
