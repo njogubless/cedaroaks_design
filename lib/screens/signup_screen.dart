d11c0eab41287c6a42b9465dbb9083929f3f0870
njogubless <pnjogubless@gmail.com>
2025-09-11 16:12:28 +0300

    modifying the signup_screen

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 
 
 class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
   @override
   _SignUpScreenState createState() => _SignUpScreenState();
 }
